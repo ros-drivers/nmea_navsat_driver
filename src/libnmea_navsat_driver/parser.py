4be9192e7d07f5245079d3a896a886f1c5922888
Loy van Beek <loy.van.beek@nobleo.nl>
2017-04-18 10:48:43 +0200

    Add various talker IDs
    According to the list at http://www.catb.org/gpsd/NMEA.html#_talker_ids

--- src/libnmea_navsat_driver/parser.py
+++ src/libnmea_navsat_driver/parser.py
@@ -126,7 +126,7 @@ parse_maps = {
 
 def parse_nmea_sentence(nmea_sentence):
     # Check for a valid nmea sentence
-    if not re.match('^\$(GP|GN).*\*[0-9A-Fa-f]{2}$', nmea_sentence):
+    if not re.match('^\$(GP|GN|GL|GA|II|IN|GB|BD|QZ).*\*[0-9A-Fa-f]{2}$', nmea_sentence):
         logger.debug("Regex didn't match, sentence not valid NMEA? Sentence was: %s"
                      % repr(nmea_sentence))
         return False
