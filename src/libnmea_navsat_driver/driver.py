5723a682be5a7ea2efaa23c28de52a987d079f5d
Griswald Brooks <griswald.brooks@gmail.com>
2017-04-29 16:15:39 -0700

    Revert "Removed autmatic prefixing of forward slash to frame_id."
    This reverts commit 81f25c59efa330a170834ca35866676d2b3fe248.

--- src/libnmea_navsat_driver/driver.py
+++ src/libnmea_navsat_driver/driver.py
@@ -177,4 +177,14 @@ class RosNMEADriver(object):
     @staticmethod
     def get_frame_id():
         frame_id = rospy.get_param('~frame_id', 'gps')
-        return frame_id
+        if frame_id[0] != "/":
+            """Add the TF prefix"""
+            prefix = ""
+            prefix_param = rospy.search_param('tf_prefix')
+            if prefix_param:
+                prefix = rospy.get_param(prefix_param)
+                if prefix[0] != "/":
+                    prefix = "/%s" % prefix
+            return "%s/%s" % (prefix, frame_id)
+        else:
+            return frame_id
