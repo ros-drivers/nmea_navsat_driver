24a9cb7a03eb0162075872842678996d118e71d1
valschmidt <vschmidt@ccom.unh.edu>
2018-09-29 18:38:14 -0400

    Add support for IMU aided GPS systems (#30)
    * Adds support for IMU aided GPS systems like the Applanix POS/MV, whose NMEA strings typically begin '$IN'. (e.g. $INGGA).
    
    * Adds support for VTG messages, which contain Course Over Ground and Speed Made Good. These are useful when not using RMC messages and you don't have a heading sensor.

--- src/libnmea_navsat_driver/parser.py
+++ src/libnmea_navsat_driver/parser.py
@@ -120,13 +120,17 @@ parse_maps = {
         ("longitude_direction", str, 6),
         ("speed", convert_knots_to_mps, 7),
         ("true_course", convert_deg_to_rads, 8),
+        ],
+    "VTG":[
+        ("true_course", safe_float,1),
+        ("speed", convert_knots_to_mps,5)
         ]
     }
 
 
 def parse_nmea_sentence(nmea_sentence):
     # Check for a valid nmea sentence
-    if not re.match('(^\$GP|^\$GN|^\$GL).*\*[0-9A-Fa-f]{2}$', nmea_sentence):
+    if not re.match('(^\$GP|^\$GN|^\$GL|^\$IN).*\*[0-9A-Fa-f]{2}$', nmea_sentence):
         logger.debug("Regex didn't match, sentence not valid NMEA? Sentence was: %s"
                      % repr(nmea_sentence))
         return False
