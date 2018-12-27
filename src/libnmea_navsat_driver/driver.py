0ea5fb61720c42870b4251b69c63617ef09e18a1
Ed Venator <evenator@gmail.com>
2018-12-27 11:58:18 -0500

    Remove automatic prefixing of forward slash to frame_id. (#33/#57)
    Removed the automatic prefixing of forward slash, but to be consistent with the current default behavior, the default frame_id has been set to /gps with the prepended forward slash.

--- src/libnmea_navsat_driver/driver.py
+++ src/libnmea_navsat_driver/driver.py
@@ -281,14 +281,11 @@ class RosNMEADriver(object):
     @staticmethod
     def get_frame_id():
         frame_id = rospy.get_param('~frame_id', 'gps')
-        if frame_id[0] != "/":
-            """Add the TF prefix"""
-            prefix = ""
-            prefix_param = rospy.search_param('tf_prefix')
-            if prefix_param:
-                prefix = rospy.get_param(prefix_param)
-                if prefix[0] != "/":
-                    prefix = "/%s" % prefix
+        """Add the TF prefix"""
+        prefix = ""
+        prefix_param = rospy.search_param('tf_prefix')
+        if prefix_param:
+            prefix = rospy.get_param(prefix_param)
             return "%s/%s" % (prefix, frame_id)
         else:
             return frame_id
