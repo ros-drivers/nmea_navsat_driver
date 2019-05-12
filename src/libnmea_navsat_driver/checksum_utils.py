e541a7884dc7341a2f42d28df5cd96da76bbebba
Ed Venator <evenator@gmail.com>
2019-05-12 11:06:52 -0400

    Fix PEP8 Violations (#68)
    Fix #67 for ROS 1:
    
    - Use autopep8 and a few manual fixes so that all Python modules and scripts
    pass `pycodestyle --max-line-length 120 src/libnmea_navsat_driver/ scripts/*`
    - Add setup.cfg file that specifies pycodestyle settings

--- src/libnmea_navsat_driver/checksum_utils.py
+++ src/libnmea_navsat_driver/checksum_utils.py
@@ -35,11 +35,12 @@
 def check_nmea_checksum(nmea_sentence):
     split_sentence = nmea_sentence.split('*')
     if len(split_sentence) != 2:
-        #No checksum bytes were found... improperly formatted/incomplete NMEA data?
+        # No checksum bytes were found... improperly formatted/incomplete NMEA
+        # data?
         return False
     transmitted_checksum = split_sentence[1].strip()
 
-    #Remove the $ at the front
+    # Remove the $ at the front
     data_to_checksum = split_sentence[0][1:]
     checksum = 0
     for c in data_to_checksum:
