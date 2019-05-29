93f9588de47d6e45bdc3875a0305d5d8775b3363
Ed Venator <evenator@gmail.com>
2019-05-28 21:06:39 -0400

    Refactor all nodes into entrypoint scripts (#76)
    Move all of the actual code for each node into its own module
    in a new "nodes" package within libnmea_navsat_driver.
    
    Make each script only 2 lines: import and call main().
    
    This will reduce the difference between ROS 1 and ROS 2 code,
    because ROS 2 uses Python entry points to install executables.
    
    Addresses #75

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages=['libnmea_navsat_driver'],
+    packages=['libnmea_navsat_driver', 'libnmea_navsat_driver.nodes'],
     package_dir={'': 'src'}
 )
 
