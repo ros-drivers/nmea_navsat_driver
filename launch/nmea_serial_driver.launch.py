fb642a3a820f12bca7854c1ba12315ad420f134c
Miro Voellmy <miro.voellmy@esa.int>
2020-09-02 18:40:37 +0200

    Update launch file for ROS2 Foxy (#110)
    * Remove get_default_launch_description, which no longer exists.
    * Update the parameters of the Node initializer to the Foxy API.

--- launch/nmea_serial_driver.launch.py
+++ launch/nmea_serial_driver.launch.py
@@ -19,14 +19,16 @@ import sys
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription, LaunchIntrospector, LaunchService
-from launch_ros import actions, get_default_launch_description
+from launch_ros import actions
 
 
 def generate_launch_description():
     """Generate a launch description for a single serial driver."""
     config_file = os.path.join(get_package_share_directory("nmea_navsat_driver"), "config", "nmea_serial_driver.yaml")
     driver_node = actions.Node(
-        package='nmea_navsat_driver', node_executable='nmea_serial_driver', output='screen',
+        package='nmea_navsat_driver',
+        executable='nmea_serial_driver',
+        output='screen',
         parameters=[config_file])
 
     return LaunchDescription([driver_node])
@@ -45,7 +47,6 @@ def main(argv):
     print('')
 
     ls = LaunchService()
-    ls.include_launch_description(get_default_launch_description(prefix_output_with_name=False))
     ls.include_launch_description(ld)
     return ls.run()
 
