d37bf1231dde7e57736765f1244100ce2abdf205
Fetullah Atas <fetulahatas1@gmail.com>
2021-08-24 15:56:41 +0200

    fix quat calculation for heading

--- src/libnmea_navsat_driver/driver.py
+++ src/libnmea_navsat_driver/driver.py
@@ -264,10 +264,10 @@ class Ros2NMEADriver(Node):
                 current_heading.header.stamp = current_time
                 current_heading.header.frame_id = frame_id
                 q = quaternion_from_euler(0, 0, math.radians(data['heading']))
-                current_heading.quaternion.x = q[0]
-                current_heading.quaternion.y = q[1]
-                current_heading.quaternion.z = q[2]
-                current_heading.quaternion.w = q[3]
+                current_heading.quaternion.w = q[0]
+                current_heading.quaternion.x = q[1]
+                current_heading.quaternion.y = q[2]
+                current_heading.quaternion.z = q[3]
                 self.heading_pub.publish(current_heading)
         else:
             return False
