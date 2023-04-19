5e3e907e3fb8212a5c2243ebd3c8a82f65c459fe
Michal Sojka <michal.sojka@cvut.cz>
2023-04-19 16:53:25 +0200

    Add non-broken implementation of pySerial read()
    This commit copies the implementation of seria.Serial.read() and
    removes one line from it: "if timeout.expired():". With this change,
    the function provides classical POSIX semantics of read(), which
    returns as soon as some bytes are received. The implementation in
    pySerial either waits for the full buffer or timeout expiration -
    nothing in between. This means that with pySerial, you either pay big
    overherd by reading character-by-character or have increased latency
    due to waiting.
    
    Unfortunately, there is no way to fix this in pySerial without
    changing the semantics and possibly breaking some applications or
    introducing a fixed function with a different name. Also, it seems
    that pySerial is no loger maintained (last commit 16 months ago, PRs
    without maintainer responses).
    
    It would be much simpler not to use pySerial and use /dev/ttyXXX
    directly. This project does not need the features offered by pySerial
    (mainly abort read from another thread). The only problem of not using
    pySerial would be lower portability.

--- src/libnmea_navsat_driver/nodes/nmea_serial_driver.py
+++ src/libnmea_navsat_driver/nodes/nmea_serial_driver.py
@@ -32,10 +32,70 @@
 
 import serial
 import io
+import os
+import select
 
 import rclpy
 
 from libnmea_navsat_driver.driver import Ros2NMEADriver
+
+class FixedSerial(serial.Serial):
+    # seria.Serial.read() with removed "if timeout.expired():" line.
+    # With this change, the function provides classical POSIX
+    # semantics of read(), which returns as soon as some bytes are
+    # received. The implementation in pySerial either waits for the
+    # full buffer or timeout expiration - nothing in between. This
+    # means that with pySerial, you either pay big overherd by reading
+    # character-by-character or have increased latency due to waiting
+    # for timeout expiration.
+    def read(self, size=1):
+        """\
+        Read size bytes from the serial port. If a timeout is set it may
+        return less characters as requested. With no timeout it will block
+        until the requested number of bytes is read.
+        """
+        if not self.is_open:
+            raise PortNotOpenError()
+        read = bytearray()
+        timeout = serial.Timeout(self._timeout)
+        while len(read) < size:
+            try:
+                ready, _, _ = select.select([self.fd, self.pipe_abort_read_r], [], [], timeout.time_left())
+                if self.pipe_abort_read_r in ready:
+                    os.read(self.pipe_abort_read_r, 1000)
+                    break
+                # If select was used with a timeout, and the timeout occurs, it
+                # returns with empty lists -> thus abort read operation.
+                # For timeout == 0 (non-blocking operation) also abort when
+                # there is nothing to read.
+                if not ready:
+                    break   # timeout
+                buf = os.read(self.fd, size - len(read))
+            except OSError as e:
+                # this is for Python 3.x where select.error is a subclass of
+                # OSError ignore BlockingIOErrors and EINTR. other errors are shown
+                # https://www.python.org/dev/peps/pep-0475.
+                if e.errno not in (errno.EAGAIN, errno.EALREADY, errno.EWOULDBLOCK, errno.EINPROGRESS, errno.EINTR):
+                    raise SerialException('read failed: {}'.format(e))
+            except select.error as e:
+                # this is for Python 2.x
+                # ignore BlockingIOErrors and EINTR. all errors are shown
+                # see also http://www.python.org/dev/peps/pep-3151/#select
+                if e[0] not in (errno.EAGAIN, errno.EALREADY, errno.EWOULDBLOCK, errno.EINPROGRESS, errno.EINTR):
+                    raise SerialException('read failed: {}'.format(e))
+            else:
+                # read should always return some data as select reported it was
+                # ready to read when we get to this point.
+                if not buf:
+                    # Disconnected devices, at least on Linux, show the
+                    # behavior that they are always ready to read immediately
+                    # but reading returns nothing.
+                    raise SerialException(
+                        'device reports readiness to read but returned no data '
+                        '(device disconnected or multiple access on port?)')
+                read.extend(buf)
+                break
+        return bytes(read)
 
 
 def main(args=None):
@@ -48,7 +108,7 @@ def main(args=None):
     serial_baud = driver.declare_parameter('baud', 4800).value
 
     try:
-        GPS = serial.Serial(port=serial_port, baudrate=serial_baud, timeout=2)
+        GPS = FixedSerial(port=serial_port, baudrate=serial_baud, timeout=2)
         driver.get_logger().info("Successfully connected to {0} at {1}.".format(serial_port, serial_baud))
         try:
             data = bytearray()
